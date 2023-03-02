3dc5d563ccb7b131d5d2ae3c4ddffce481204987
Jakub Josef Forman <me@jakubforman.eu>
2023-03-02 13:46:03 +0100

    Create API communication with models
    - Create PostModel
    - Add model with Generics in PostService
    - Add get method for single Post
    - Add Tab1 (Posts) view

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
+import {PostModel} from "../../models/post.model";
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,17 @@ export class PostService {
   index$() {
     // alternative
     // return this.http.get(environment.baseUrl + "/posts");
-    return this.http.get(`${environment.baseUrl}/posts`)
+    return this.http.get<PostModel[]>(`${environment.baseUrl}/posts`)
+  }
+
+  /**
+   * Get post
+   * 
+   * Get single post
+   * 
+   * @param id Post ID
+   */
+  get$(id: number) {
+    return this.http.get<PostModel>(`${environment.baseUrl}/posts/${id}`)
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {PostService} from "../services/post/post.service";
+import {Observable} from "rxjs";
+import {PostModel} from "../models/post.model";
 
 @Component({
   selector: 'app-tab1',
@@ -8,11 +10,25 @@ import {PostService} from "../services/post/post.service";
 })
 export class Tab1Page {
 
+  posts$: Observable<PostModel[]>;
+
+  // alternativní způsob výpisu
+  posts: PostModel[] = [];
+
   constructor(
     private postService: PostService
   ) {
+    // předám observable příspěvků
+    this.posts$ = this.postService.index$();
+
+    // alternativní zápis bez pipe | async na view
     this.postService.index$().subscribe(posts => {
-      console.log(posts);
+      this.posts = posts;
+    })
+
+    // Ukázkový výpis do konzole
+    this.postService.index$().subscribe(posts => {
+      console.log(posts[0].title);
     })
   }
 
