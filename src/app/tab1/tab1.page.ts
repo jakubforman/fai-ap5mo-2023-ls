17b38e7065eac7374126c7fefb46058cf42ca436
Jakub Josef Forman <me@jakubforman.eu>
2023-03-09 13:45:42 +0100

    Adding filtration

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {PostService} from "../services/post/post.service";
-import {Observable} from "rxjs";
+import {Observable, ReplaySubject} from "rxjs";
 import {PostModel} from "../models/post.model";
 
 @Component({
@@ -10,26 +10,46 @@ import {PostModel} from "../models/post.model";
 })
 export class Tab1Page {
 
-  posts$: Observable<PostModel[]>;
+  // Vytvořím subject pro vlastní observable
+  private postsSubject = new ReplaySubject<PostModel[]>(1);
 
-  // alternativní způsob výpisu
-  posts: PostModel[] = [];
+  get posts$() {
+    return this.postsSubject.asObservable();
+  }
 
   constructor(
     private postService: PostService
   ) {
-    // předám observable příspěvků
-    this.posts$ = this.postService.index$();
-
-    // alternativní zápis bez pipe | async na view
     this.postService.index$().subscribe(posts => {
-      this.posts = posts;
-    })
-
-    // Ukázkový výpis do konzole
-    this.postService.index$().subscribe(posts => {
-      console.log(posts[0].title);
+      this.postsSubject.next(posts);
     })
   }
 
+  /**
+   * Filter posts by title
+   *
+   * Use index$()
+   *
+   * @param event
+   */
+  handleChange(event: any) {
+    // kontrola pokud value nic neobsahuje, dej mi všechny příspěvky
+    if (!event.detail.value) {
+      this.postService.index$().subscribe(posts => {
+        this.postsSubject.next(posts);
+      })
+      return;
+    }
+
+    // získat posty
+    this.postService.index$().subscribe(posts => {
+      // filtrace postů podle titulku
+      const filteredPosts = posts.filter(post => {
+        return post.title.toLowerCase().indexOf(event.detail.value.toLowerCase()) > -1;
+      });
+
+      // předání vyfiltrovaných dat do subjectu
+      this.postsSubject.next(filteredPosts);
+    });
+  }
 }
